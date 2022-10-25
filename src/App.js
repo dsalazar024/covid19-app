23667a58c09a30d29847f2b50bd11f1e1742f3c8
Dylan <dylan.salazar@blautech.us>
2022-10-25 14:00:10 -0600

    remove card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Cards from './components/Cards/Cards';
 import TableStates from './components/States/TableStates';
 import styles from './App.module.css';
 import Charts from './components/Chart/Chart';
@@ -11,7 +10,6 @@ class App extends React.Component {
             <div container >
                 <div>
                     <h1>Covid-App-Genpac</h1>
-                    {/* <Cards /> */}
                 </div>
                 <div>
                     <TableStates />
